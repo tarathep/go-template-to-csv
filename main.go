b7d530246439278b6459fdc6385646a75ffbce2c
tarathep <bokie.tarathep@gmail.com>
2022-07-22 11:59:17 +0700

    init project

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.New()
+	t, err := loadTemplate()
+	if err != nil {
+		panic(err)
+	}
+	r.SetHTMLTemplate(t)
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "/assets/index.tmpl", gin.H{
+			"Foo": "World",
+		})
+	})
+	r.GET("/bar", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "/assets/bar.tmpl", gin.H{
+			"Bar": "World",
+		})
+	})
+
+	r.GET("/template", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "/assets/template.tmpl", gin.H{
+			"Bar": "World",
+		})
+	})
+
+	r.GET("/table", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "/assets/table.tmpl", gin.H{
+			"Bar": "World",
+		})
+	})
+
+	r.GET("/assets/:filename", func(c *gin.Context) {
+		file, err := Assets.Open("/assets/" + c.Param("filename"))
+		if err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+		content, err := ioutil.ReadAll(file)
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+		// c.Data(http.StatusOK, "application/javascript", content)
+		c.Data(http.StatusOK, "text/css", content)
+	})
+	r.Run(":8080")
+}
+
+func loadTemplate() (*template.Template, error) {
+	t := template.New("")
+	for name, file := range Assets.Files {
+		if file.IsDir() || !strings.HasSuffix(name, ".tmpl") {
+			continue
+		}
+		h, err := ioutil.ReadAll(file)
+		if err != nil {
+			return nil, err
+		}
+		t, err = t.New(name).Parse(string(h))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return t, nil
+}
---
+++ run.bat
@@ -0,0 +1,3 @@
+go-assets-builder assets -o assets.go
+go build
+.\go-template-to-csv.exe
